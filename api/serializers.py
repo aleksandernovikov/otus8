e5d40d2a1e546b9c520888636afeb27f2b6f1396
sanya <79fz270704@gmail.com>
2020-02-19 16:03:27 +0300

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
